9c59025233b7d1bb4911c8107c03de09dc79c578
Hanna <hanna@Hannas-Laptop.local>
2023-01-26 15:03:06 +0000

    Learning fresh :)

---
+++ routes/countdown.tsx
@@ -0,0 +1,15 @@
+
+import Countdown from "../islands/Countdown.tsx";
+
+export default function Page() {
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + 1)
+    return (
+        <>
+        <p>
+            The big event is happening!
+        </p>
+        <Countdown target={date.toISOString()} />
+        </>
+    )
+}
---
+++ routes/index.tsx
@@ -0,0 +1,23 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div>
+        <img
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p>
+          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+      </div>
+    </>
+  );
+}
